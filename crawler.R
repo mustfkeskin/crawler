bb28c6ee77c00bb2e252c61afd190139760be4e2
mustfkeskin <mustfkeskin@users.noreply.github.com>
2017-08-13 13:25:48 +0300

    Create crawler.R

---
+++ crawler.R
@@ -0,0 +1,103 @@
+rm(list = ls())
+library(data.table)
+library(XML)
+library(rvest)
+
+collect <- function(sublinks){
+
+  newdata <- c()
+  for(i in 1:length(sublinks)){
+    Sahibinden <- read_html(sublinks[i])
+    print(paste(sublinks[i]))
+    attribute <- Sahibinden %>% html_nodes(".classifiedInfoList") %>% html_nodes("li") %>% html_nodes("strong") %>% html_text()
+    attribute <- gsub("[\t\r\n\ ]", "", attribute)
+    value     <- Sahibinden %>% html_nodes(".classifiedInfoList") %>% html_nodes("li") %>% html_nodes("span") %>% html_text()
+    value     <- gsub("[\t\r\n\ ]", "", value)
+
+
+    result = tryCatch({
+      newdata    <- rbind(newdata, t(value))
+    }, warning = function(w) {
+      warning-handler-code
+    }, error = function(e) {
+      print(paste(e))
+    })
+
+
+    if(i == 1){
+      colnames(newdata) <- attribute
+    }
+  }
+  return(newdata)
+}
+
+
+
+
+
+url <- "https://www.sahibinden.com/satilik"
+sahibinden <- read_html(url)
+
+sublinks     <- sahibinden %>% html_nodes(".classifiedTitle") %>% html_attr("href")
+emlak_tipi   <- sahibinden %>% html_nodes(".searchResultsTagAttributeValue") %>% html_text()
+ilan_basligi <- sahibinden %>% html_nodes(".classifiedTitle") %>% html_text()
+m2_oda       <- sahibinden %>% html_nodes(".searchResultsAttributeValue") %>% html_text()
+tek          <- seq(1, length(m2_oda), 2)
+cift         <- seq(2, length(m2_oda), 2)
+m2           <- m2_oda[tek]
+oda          <- m2_oda[cift]
+ilan_tarihi  <- sahibinden %>% html_nodes(".searchResultsDateValue") %>% html_text()
+fiyat        <- sahibinden %>% html_nodes(".searchResultsPriceValue") %>% html_text()
+location     <- sahibinden %>% html_nodes(".searchResultsLocationValue") %>% html_text()
+
+pos = grep('/projeler/p', sublinks)
+if(length(pos) != 0){
+  ilan_basligi <- ilan_basligi[-pos]
+  sublinks     <- sublinks[-pos]
+}
+
+
+mydata        <- data.table(cbind(emlak_tipi, ilan_basligi, m2, oda, ilan_tarihi, fiyat, location))
+allSubLinks   <- paste("https://www.sahibinden.com" ,sublinks,sep = "")
+newAttributes <- collect(allSubLinks)
+mydata        <- cbind(mydata, newAttributes)
+
+nextPage <- length(m2)
+
+while(TRUE){
+
+  nextUrl  <- "?pagingOffset="
+  newUrl   <- paste(url, nextUrl, toString(nextPage), sep = "")
+  nextPage <- nextPage + length(m2)
+
+  sahibinden <- read_html(newUrl)
+  print(paste("------------------"))
+  print(paste(newUrl))
+
+  sublinks     <- sahibinden %>% html_nodes(".classifiedTitle") %>% html_attr("href")
+  emlak_tipi   <- sahibinden %>% html_nodes(".searchResultsTagAttributeValue") %>% html_text()
+  ilan_basligi <- sahibinden %>% html_nodes(".classifiedTitle") %>% html_text()
+  m2_oda       <- sahibinden %>% html_nodes(".searchResultsAttributeValue") %>% html_text()
+  tek          <- seq(1, length(m2_oda), 2)
+  cift         <- seq(2, length(m2_oda), 2)
+  m2           <- m2_oda[tek]
+  oda          <- m2_oda[cift]
+  ilan_tarihi  <- sahibinden %>% html_nodes(".searchResultsDateValue") %>% html_text()
+  fiyat        <- sahibinden %>% html_nodes(".searchResultsPriceValue") %>% html_text()
+  location     <- sahibinden %>% html_nodes(".searchResultsLocationValue") %>% html_text()
+
+  pos = grep('/projeler/p', sublinks)
+  if(length(pos) != 0){
+    ilan_basligi <- ilan_basligi[-pos]
+    sublinks     <- sublinks[-pos]
+  }
+
+  mydataNew     <- data.table(cbind(emlak_tipi, ilan_basligi, m2, oda, ilan_tarihi, fiyat, location))
+  allSubLinks   <- paste("https://www.sahibinden.com" ,sublinks,sep = "")
+  newAttributes <- collect(allSubLinks)
+  mydataNew     <- cbind(mydataNew, newAttributes)
+  mydata        <- rbind(mydata, mydataNew)
+
+  print(paste(dim(mydata)))
+
+}
